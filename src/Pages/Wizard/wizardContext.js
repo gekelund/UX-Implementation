af07d16540fd3d6f36b255c9c4548641f4017ec9
gekelund <59283488+gekelund@users.noreply.github.com>
2020-09-24 09:49:28 +0200

    base wizzard and pages added (#1)

---
+++ src/Pages/Wizard/wizardContext.js
@@ -0,0 +1,18 @@
+import React, {
+    useState, 
+    useReducer, 
+    useContext, 
+    createContext,
+} from 'react';
+import DeliveryInfo from './deliveryInfo';
+
+const WizardContext = () => {
+
+   return (
+       <div>
+           Wizard
+       </div>
+   )
+}
+
+export default WizardContext;
--- src/Routes/index.js
+++ src/Routes/index.js
@@ -3,15 +3,18 @@ import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import LandingPage from '../Pages/LandingPage';
 import SignInPage from '../Pages/SignInPage';
+import Wizard from '../Pages/Wizard';
 
 export const LANDING = '/';
 export const SIGN_IN = '/signin';
+export const WIZARD = '/wizard';
 
 const Routes = () => (
     <Router>
         <Switch>
             <Route exact path={LANDING} component={LandingPage} />
             <Route path={SIGN_IN} component={SignInPage} />
+            <Route path={WIZARD} component={Wizard} />
         </Switch>
     </Router>
 );
