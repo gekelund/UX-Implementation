286dd9f2b95f1b3084c2f157d5f1924cac5cb4d7
Gustaf Ekelund <gustafaekelund@gmail.com>
2020-10-01 14:41:22 +0200

    small fixes

--- src/Pages/LandingPage/index.js
+++ src/Pages/LandingPage/index.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import SoupCard from '../../Components/SoupCard';
 
+import styled from "styled-components";
+import tw from "tailwind.macro";
+
+const LandingStyling = styled.div.attrs({
+    className: "w-full h-screen flex flex-col flex-wrap items-center justify-center",
+  })`
+    & {
+        main {
+            ${tw`shadow-lg m-6 p-5 h-auto flex flex-wrap flex-col items-center justify-center rounded-md`}
+        }
+        
+       
+    }
+  `;
+
+ 
+
 const LandingPage = () => {
 
     return (
-        <div>
+        <LandingStyling>
+            <main>
             <h1>LandingPage</h1>
             <SoupCard />
-        </div>
+            </main>
+        </LandingStyling>
     )
 }
 
